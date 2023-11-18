b3f0aee7b9c9091de5b274fca5de8d1618ae8300
ireneuszcierpisz <>
2023-11-18 22:22:07 +0000

    Add one more example of reduce method

--- map-filter-reduce.js
+++ map-filter-reduce.js
@@ -86,7 +86,7 @@ console.log('candidates: ', candidates);
 
 
 // reduce method
-let reducedNums = nums.reduce((acc, curr) => acc + curr, 0); //explicitly specifying the initial value of zero.
+let reducedNums = nums.reduce((acc, curr) => acc + curr, 0);//explicitly specifying the initial value of zero.
 console.log(reducedNums); // 55
 
 let team = [
@@ -143,3 +143,49 @@ let nameByProfession = team.reduce((acc, curr) => {
     return acc;
 }, {});
 console.log(nameByProfession); //{ dev: [ 'an', 'ar' ], des: [ 'm', 'k' ], manager: [ 'l' ] }
+
+
+//-------------------------------------
+let newstudents = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 73, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 93, english: 88, maths: 97, art: 95},
+    },
+    {
+        name: 'Fran',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 87, art: 95},
+    }
+];
+
+const biggest = newstudents.reduce((acc, curr) => {
+    // let {name, results} = curr;
+    // if (!acc['name']) {
+    //     acc['name'] = name;
+    //     acc['max'] = results.english;
+    // } else {
+    //     if (results.english > acc['max']) {
+    //         acc['name'] = name;
+    //         acc['max'] = results.english;
+    //     }
+    // }
+    acc = acc.max > curr.results.english ? acc: {name:curr.name, max:curr.results.english};
+
+    return acc
+}, {name: '', max: 0});
+console.log(biggest);  // { name: 'Adam', max: 88 }
